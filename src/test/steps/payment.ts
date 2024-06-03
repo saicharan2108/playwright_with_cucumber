07a7db6163d2ebea64922964205931c23d8a1bd7
Sai Charan <92617073+saicharan2108@users.noreply.github.com>
2024-06-03 11:11:39 +0530

    Update payment.ts

--- src/test/steps/payment.ts
+++ src/test/steps/payment.ts
@@ -1,46 +1,45 @@
-import { Given, When, Then , setDefaultTimeout} from '@cucumber/cucumber';
+import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
-import { Browser, Page, chromium  } from "@playwright/test"
+import { Browser, Page, chromium } from 'playwright';
 import { HomePage } from '../../../pages/home.po';
+
 let homePage: HomePage;
-let page: Page
-let browser: Browser
+let page: Page;
+let browser: Browser;
 setDefaultTimeout(2000000); 
 
 Given('User navigates to the application', async function () {
-  browser = await chromium.launch({ headless: false })
-  const context = await browser.newContext()
-  page = await context.newPage()
-  homePage = new HomePage(page)
-  await page.goto('https://uat.onesingleview.com/login', { timeout: 600000 }); 
+  browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  page = await context.newPage();
+  homePage = new HomePage(page);
+  await page.goto('https://uat.onesingleview.com/login', { timeout: 60000 });
   await homePage.verifyLoginPage();
-
 });
 
-Given('User log in with {string} and {string}', async (username: string, password: string)=> {
+Given('User log in with {string} and {string}', async function (username: string, password: string) {
   await homePage.enterUsername(username);
   await homePage.enterPassword(password);
   await homePage.clickLogin();
   await homePage.verifyHomePage();
 });
 
-When('User navigates to the payments tab', async () => {
+When('User navigates to the payments tab', async function () {
   await homePage.navigateToPaymentsTab();
   await homePage.verifyPaymentTab();
-
 });
 
-When('User clicks on the transfer sub-nav item', async () => {
+When('User clicks on the transfer sub-nav item', async function () {
   await homePage.clickOnTransfer();
   await homePage.verifyTransfer();
 });
 
-When('User clicks on the beneficiary sub-nav item', async () => {
+When('User clicks on the beneficiary sub-nav item', async function () {
   await homePage.clickOnBeneficiary();
   await homePage.verifyBeneficiary();
 });
 
-When('User clicks on create', async ()=> {
+When('User clicks on create', async function () {
   await homePage.clickOnCreatePayment();
   await homePage.verifyCreatePayment();
 });
@@ -53,9 +52,8 @@ When('User submits the payment', async function () {
   await homePage.submitPayment();
 });
 
-
 Then('The payment should be listed in the payments page', async function () {
   const isListed = await homePage.isPaymentListed();
   expect(isListed).toBe(true);
-  browser.close();
+  await browser.close();
 });
